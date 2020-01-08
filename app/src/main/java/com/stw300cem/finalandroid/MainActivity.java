fc73be9ea87b8e307a013820c5a6ca7e87925c50
sumanbhujel <xnamus@outlook.com>
2020-01-08 08:50:23 +0545

    initial

---
+++ app/src/main/java/com/stw300cem/finalandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.stw300cem.finalandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
