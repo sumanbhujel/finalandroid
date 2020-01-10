0b3d9cee29319b7fe6f8ddde271690f93e9bd1dc
sumanbhujel <xnamus@outlook.com>
2020-01-10 14:01:35 +0545

    SignUpActivity added

---
+++ app/src/main/java/com/stw300cem/finalandroid/activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.stw300cem.finalandroid.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.stw300cem.finalandroid.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
