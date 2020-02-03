796610d27cc6bf1006f7a9d73f32f8209839ecb8
sumanbhujel <xnamus@outlook.com>
2020-02-03 11:19:51 +0545

    ProfileActivity added

---
+++ app/src/main/java/com/stw300cem/finalandroid/activities/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.stw300cem.finalandroid.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
